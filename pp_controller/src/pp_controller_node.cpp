f923d31acf14ae1899cf6af1a52e95f9a712207c
TomasPedreira <ta.pedreira@campus.fct.unl.pt>
2025-08-04 13:06:26 +0100

    put negative sign in angular because why not

--- pp_controller/src/pp_controller_node.cpp
+++ pp_controller/src/pp_controller_node.cpp
@@ -321,7 +321,7 @@ geometry_msgs::msg::TwistStamped PPController::computeVelocityCommands(
   }
 
   cmd_vel.twist.linear.x = linear_speed;
-  cmd_vel.twist.angular.z = std::clamp(desired_angular_speed, -max_angular_speed_, max_angular_speed_);
+  cmd_vel.twist.angular.z = -std::clamp(desired_angular_speed, -max_angular_speed_, max_angular_speed_);
 
   is_initialized_ = false;
   return cmd_vel;
